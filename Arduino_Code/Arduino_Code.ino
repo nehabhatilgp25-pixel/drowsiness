31ebc5228274422b88528c9c482bf99f3ac6137a
nehabhatilgp25-pixel <neha.bhati_lgp25@ashoka.edu.in>
2025-11-23 07:46:00 +0530

    Update Arduino_Code.ino

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -4,7 +4,7 @@ NinjaIoT iot;
 
 void setup() {
   Serial.begin(115200);
-  iot.connect("MSB_2G", "9900015169", "NB09");   //link: https://iot.roboninja.in/
+  iot.connect("wifi", "pwd", "uid");   //link: https://iot.roboninja.in/
 }
 
 void loop() {
